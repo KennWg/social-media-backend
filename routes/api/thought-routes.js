fd3c5520d60d8760b960808bd5324adcb9ed7ee7
KW <kenneth_sl_wong@msn.com>
2022-04-22 17:25:39 -0400

    added routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').router();
+
+router
+    .route('/')
+    .get()
+    .post();
+
+router
+    .route('/:thoughtId')
+    .get()
+    .put()
+    .delete();
+
+router
+    .route('/:thoughtId/reactions/')
+    .post();
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete();
+
+module.exports = router;
